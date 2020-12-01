3d4e8a59eef151d87a299d598c6c6c7725e8df30
Roxana Isadora <roxanaisadora.gmail.com>
2020-11-30 23:03:24 -0500

    routes of loggin

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ import '../assets/styles/components/Search.scss'
 const Search = () => (
     <section className="main">
        <h2 className="main__title">Categorias</h2>
-       <input className="input" type="text" placeholder="Buscar..."/>
+       <input className="search__input" type="text" placeholder="Buscar..."/>
    </section>
 )
 
---
+++ src/containers/Login.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../assets/styles/components/Login.scss';
+
+const Login = () => (
+    <section className="login">
+        <section className="login_container">
+            <h2 tabIndex="0">Iniciar Sección</h2>
+            <form className ="login__conteiner--form">
+                <input aria-label="correo" className="login__input" type="text" placeholder="Correo Electronico"/>
+                <input aria-label="contraseña" className="login__input" type="password" placeholder="contraseña"/>
+                <button className="button">Iniciar sección</button>
+                <div className="login__conteiner--remember-me">
+                    <label>
+                        <input type="checkbox" name="" id ="cbox1" value="checkbox"/>Recuerdamé
+                    </label>
+                    <label>
+                        <a href="/">Olvidé mi contraseña</a>
+                    </label>
+                </div>
+            </form>
+            <section className="login__conteiner--social-media">
+                <div><img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="google" />Iniciar sección con Google</div>
+                <div><img src="https://img.icons8.com/fluent/48/000000/twitter.png" alt="twetter"/>Iniciar sección con Twetter</div>
+            </section>
+            <p className="login__conteiner--register">Si no tienes una cuenta  <a href=""> Registrate</a></p>
+        </section>
+    </section>
+)
+
+export default Login
