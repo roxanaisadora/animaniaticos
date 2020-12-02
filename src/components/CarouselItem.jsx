341d5f27d5b941d29e780b193d5a001642074f8e
Roxana Isadora <roxanaisadora.gmail.com>
2020-12-01 19:00:40 -0500

    Creating the reducers for favorites

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {setFavorite} from '../actions'
 import '../assets/styles/components/CarouselItem.scss';
 import play from "../assets/static/play.png";
 import add from "../assets/static/add.png";
 
-const CarouselItem = ({cover, title, year, contentRating, duration}) => (
-    <div className="carousel-item">
-            <img className="carousel-item__img" src={cover} alt={title}/>
-            <div className="carousel-item__details">
-                <div>
-                    <img src={add}  alt="add"/>
-                    <img src={play} alt="play"/>
+const CarouselItem = (props) => {
+    const {cover, title, year, contentRating, duration} = props;
+    const handleSetFavorite = () => {
+       props.setFavorite(
+           { cover, title, year, contentRating, duration}
+       )
+    }   
+    return(
+        <div className="carousel-item">
+                <img className="carousel-item__img" src={cover} alt={title}/>
+                <div className="carousel-item__details">
+                    <div>
+                        <img src={add}  alt="add" onClick={handleSetFavorite} />
+                        <img src={play} alt="play"/>
+                    </div>
+                    <p className="carousel-item__details--title">{title}</p>
+                    <p className="carousel-item__details--subtitle">{`${year} ${contentRating} ${duration}`}</p>
                 </div>
-                <p className="carousel-item__details--title">{title}</p>
-                <p className="carousel-item__details--subtitle">{`${year} ${contentRating} ${duration}`}</p>
             </div>
-        </div>
-)
+    )
+}
 CarouselItem.propTypes = {
     cover: PropTypes.string,
     title: PropTypes.string,
@@ -24,4 +34,7 @@ CarouselItem.propTypes = {
     contentRating: PropTypes.string,
     duration: PropTypes.number
 }
-export default CarouselItem
+const mapDispatchToProps = {
+    setFavorite,
+}
+export default connect(null, mapDispatchToProps)(CarouselItem)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,13 @@
-const reducer = (state,action) => {
-    return state
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_FAVORITE':
+            return {
+                ...state,
+                mylist: [...state.mylist, action.payload]
+            }
+        default:
+            return state;
+    }
 }
 
 export default reducer
